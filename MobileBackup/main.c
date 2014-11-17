501e2c6e5df3c4c7d3dbc9e75a83901b61d8f07e
ssgreg <me@ssgreg.com>
2014-11-17 10:35:40 +0300

    Initial Commit

---
+++ MobileBackup/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MobileBackup
+//
+//  Created by 0xGreg on 17/11/14.
+//  Copyright (c) 2014 Grigory Zubankov. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+    return 0;
+}
