9f272f6834210b0cce3c3fe2f9d00ad37a454a67
ssgreg <me@ssgreg.com>
2014-11-17 11:54:58 +0300

    trying to compile

--- MobileBackup/main.c
+++ MobileBackup/main.c
@@ -8,8 +8,8 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-  // insert code here...
-  printf("Hello, World!\n");
-    return 0;
+
+int main(int argc, const char * argv[])
+{
+  return 0;
 }
