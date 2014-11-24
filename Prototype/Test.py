d3c9025aec845dad31c35814bbd28ff9dfdea64e
ssgreg <me@ssgreg.com>
2014-11-24 13:39:26 +0300

    stored pair record data (plus example)

--- Prototype/Test.py
+++ Prototype/Test.py
@@ -422,6 +422,7 @@ class TestConnectToLockdown(object):
   def __init__(self, io_service, did, sn):
     self.did = did
     self.sn = sn
+    self.pair_record_data = None
     self.connection = Connection(io_service, connect())
     self.internal_session = UsbMuxPlistSession(self.connection)
     #
@@ -448,6 +449,7 @@ class TestConnectToLockdown(object):
       # self.internal_session.send(create_plist_get_pair_record(self.sn), self.on_get_pair_record)
 
   def on_get_pair_record(self, result):
+    self.pair_record_data = plist.loads(result.PairRecordData.data.decode('utf-8'))
     logger().debug('Connecting to lockdown...')
     self.internal_session.send(create_usbmux_plist_connect(self.did, 32498), self.on_connect_to_lockdown)
 
@@ -518,3 +520,114 @@ Main()
 
 # query service type:
 # {'Type': 'com.apple.mobile.lockdown', 'Request': 'QueryType'}
+
+# read pair data
+#   {'RootPrivateKey':
+#     Data('-----BEGIN RSA PRIVATE KEY-----
+#     MIIEowIBAAKCAQEAsZd2T8AgjWz43D+QGLaT7linU4PvvcpPeLfb/yWMtuRRj3nn
+#     j4ObURsOVt/wh4sILIMGT9gmzB6S3L4QYdv982jox3fbDRJOoLO1XgTSI/wLhLfS
+#     fZFOtz2mH8uALAMQtjTtz8JRgrAC3ttbM8JqeXL88MI3rzx8OnJ/+ZAJa3sUBQW7
+#     sLruC6gk8qJDg68iAbRhGhKAGzXgy0y2oXdRxraNsuLB9+QD7SnpBVQF5dbb/Fg7
+#     1/tmrdP6oGUf7NcWbWX313WX3mOJpyvIjwQ/KHCvlJCWezDljE6uHV4cbHLBmOIQ
+#     yfMYpKGHkyIGVbzXrNPeKnHoNItvyTB2fUhU6QIDAQABAoIBAAsMPhr1+9BwgAbL
+#     mX5AFXHVaAtlAMwQwuLipTf5MQjLqVtAnSwrZsUj5r92zPLRrl9+gy0CaF94Y3CC
+#     DmjYE7NJynlmNmVYxzVuGJFRwXTloB3QhbK0EG6L0kLWGRIj2q1LG6Q6FXII2xTc
+#     Fo9gv9jwU1Xy7x3iqGPGbbmrwTpDYlJlFWWAmeFMeu6ydS4wX3nB3UUY3VCA+2VI
+#     YpIe6KVz5ecg8TrV+n/MI0VHNQfpoNK7+Pp87fwmD+81KpE3uFnb6iB4j9+9UMH7
+#     E8NUHkVioZYotNqvfCsRr519+XNrd3ZBZoAMKEkZE1YFcezQlCw64NDEN/9ILEJT
+#     pxsc+PUCgYEA2BeGsIpwAUDKy4QnfdSBIimuDNx7dK1M78MB7zAJGdjoAHwcVZsA
+#     3F75Ku6qOaLoTXkv5Fn1c2nNmf8il4szsHEZ4cmu8QDivxJDzC2JsGhcvijA1da/
+#     C28egHa61QsWCG1VgxCBcTJM+nFCiskEX6v4P1aeCB/T+fCwjdK4AtMCgYEA0mO0
+#     7nrfZv2TzmaLGwoxOQncEZ8YrsoBj+CvQRnOffs1whUg8h9GqFq5b1J5LZSU+FsL
+#     +mgdZ4zcHRzDlTrAn5dwEQGdMBunkKtlVVSdLkVz488A2r0pxt121pxtuLGdV8xd
+#     ugbjGNTj0FAZ4cHvmavl6lMqisdo7/F/AKrPW9MCgYEAz8GmtFsVHo9NqmePlHHY
+#     CaFq/mCIrX/kxWuGk5FqXphlTfoFP+S76iPBiZ5WuP1APSQzgGdhDip+Cv36Ixsp
+#     ggzrZmKCuKEyX6PO3SI3DAz6hbUm2TETEoouGDKOpD95WnbLzSv0V4GV8v8ri8WC
+#     V/09up+rZoI7SkU3adNbKRMCgYAcN89OpSBNmfk3F73UlJthgRcJLgOLfgCToZkf
+#     7Cwr7YOaUmcm0lcRwYlZiwBBwsnwPZXvsgMvnk7vgKoi28x2G0OmpEbG6w6gcryW
+#     o1CpohsXFO6SaAiwaYfE5Ro6SWhHHFv91WqkK/maOQQfqXlPbSTbNuhf0LscY5Gb
+#     L028jQKBgBHSPz7ICudpJMwLaYcbbJ1FguWuF9C4AaGUj/ZsCjtqLX0IlwjYSPtA
+#     eHEX5aNZNP8swVpGU46yAnuh3xcYa4vX14MwYFB52wnNvPs8LnLqcfDoVJjYcIcx
+#     lMkhzUI997aSFPxP7FC1f2HCg7+kBcbVHkFQU91s62VZ0juA/yN0
+#     -----END RSA PRIVATE KEY-----'),
+#   'HostCertificate': Data('-----BEGIN CERTIFICATE-----
+#     MIICujCCAaKgAwIBAgIBADANBgkqhkiG9w0BAQUFADAAMB4XDTE0MTEyMDE0MjMy
+#     MFoXDTI0MTExNzE0MjMyMFowADCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoC
+#     ggEBAPUJQ4eh9TfKQkuk7ksyGzMnJ+15kwX0PgTewVZdrH4oe31+QxOzNc1+LNQe
+#     QUilZz6RoMC2qZrmbjhkypeSPQBbeKVbd18YsP0iXfPmZ0W4npvQGCGsgJpuVgrS
+#     8IW6tWimwMtwKETqrFbGJx4HGJqwQQcvRqLLC+jIIEbvuG2Kx1wC1iwckrEUfzjk
+#     +w1akiN+T1KfbqS6WpNSzTFKCusWOWrrM0xT7oZEmhrHXRShti2qpnGxORwF6RqU
+#     TSnn30qfwEE2DizZ9Qqz4F3JwiF0KwQKmnMBNX3sgwRQXFq90tcNB2UvtHbvhWwm
+#     nNLRRFJq1vb3aAcuMNSK3By0nQ0CAwEAAaM/MD0wDAYDVR0TAQH/BAIwADAdBgNV
+#     HQ4EFgQUpTcgAL+bx478gtYGY3B+VTWEwCMwDgYDVR0PAQH/BAQDAgWgMA0GCSqG
+#     SIb3DQEBBQUAA4IBAQBUC2iVkuwS963ja7xWXWPtcc8GmV12mkKbC2efBxHnLkOL
+#     bw2beAe8wySIXRhvSs4WxsuqMl/G3M8YZ1MaakC2fpPsE51bXS8Pgttc3E2+C2sr
+#     UiTm8ftvS17TLlr/xUYlW/D0hM9T3IRRK98iH7YbSqYEQxT15IRziW8liyFtAXdK
+#     lKQKWw+OjFwukVRzV3qiNaZJfYpHz65UHkVNjTp7AiXfMZTkD2Q2cnX6QF6Vk11w
+#     nUNm86rU1ZCe0XEwt1iGxqmGLiwwGmZ56lBxMB77kjycoBYkICJcS26ehxIim7Qz
+#     fjBeLav2dLaOIhFQ2njti+xJqUcU69Vp26l89uNH
+#     -----END CERTIFICATE-----'),
+#   'HostID': '670A85A0-D184-46F1-B056-B7A15792EACD',
+#   'DeviceCertificate': Data('-----BEGIN CERTIFICATE-----
+#     MIICNjCCAR6gAwIBAgIBADANBgkqhkiG9w0BAQUFADAAMB4XDTE0MTEyMDE0MjMy
+#     MFoXDTI0MTExNzE0MjMyMFowADCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEA
+#     xEUWc3ECvEqN0PIcsBs9oHpja5CC3Ttm3ltcu52Xxv3vY5ogq0saTSzHenmiy4lF
+#     NFJ7s/PchKPEy/2J+ISPuOXBu1y4xQSk0cGH3zS38pGeLLk/jVm88FTl4xmm6gFm
+#     lDSzWpEfXdDYvbo04l4s7+ej7ltuf4viwc9fde2WKxMCAwEAAaM/MD0wDAYDVR0T
+#     AQH/BAIwADAdBgNVHQ4EFgQU7PsLUELmKkHClR6fYsFjscWgx78wDgYDVR0PAQH/
+#     BAQDAgWgMA0GCSqGSIb3DQEBBQUAA4IBAQBx/+8Pu2GPm3ieFMIhxxRgYkvcj5kt
+#     aNGV+oLJ2+ogTh38EUSnQb+s663C+vbLTB7XRRBoe2piHVIjYOTPnwMdeTHFOTnH
+#     d2JHTcOhkoULMIXOc9BavaCvWOmdU8da5UTd0qKEwKGeKWBizg7J1Rpp70k0y1Bj
+#     RNfxMLBBy2yO8RGNEPIq2paBt8N9espueqKAf+N4Z21OMFfbfvIDFy4NyQpnqgjQ
+#     7F2+1qf064NDMsT4iNNN0ANVR3s3vOgvBQWqdwtavYNwuBIVvCHKnmofqIifuFbw
+#     HQO/+WNRLoV3FHrt6yJi0XLWu7Pcz30ZR3N1+NpJVRqBEoZfOtlWOwHZ
+#     -----END CERTIFICATE-----'),
+# 'EscrowBag': Data(':0\xbdmR\xc7&\xc0\x9eu\x9d\xc0\xac\xc9}\xed`\xa0\xb9\xe7\xfcf\xb5\xf6\x8bH\xb2\xf6j\xe5<u'),
+# 'HostPrivateKey': Data('-----BEGIN RSA PRIVATE KEY-----
+#     MIIEogIBAAKCAQEA9QlDh6H1N8pCS6TuSzIbMycn7XmTBfQ+BN7BVl2sfih7fX5D
+#     E7M1zX4s1B5BSKVnPpGgwLapmuZuOGTKl5I9AFt4pVt3Xxiw/SJd8+ZnRbiem9AY
+#     IayAmm5WCtLwhbq1aKbAy3AoROqsVsYnHgcYmrBBBy9GossL6MggRu+4bYrHXALW
+#     LBySsRR/OOT7DVqSI35PUp9upLpak1LNMUoK6xY5auszTFPuhkSaGsddFKG2Laqm
+#     cbE5HAXpGpRNKeffSp/AQTYOLNn1CrPgXcnCIXQrBAqacwE1feyDBFBcWr3S1w0H
+#     ZS+0du+FbCac0tFEUmrW9vdoBy4w1IrcHLSdDQIDAQABAoIBADMR0jRDihMtoXGY
+#     l12uvMKFh6nT69OS2xYywwLYFSpaD5rRJPPHJxCEGp2/DIYyivkcau+fYmv5WMGC
+#     KEM85F/dsKBuFswIHmMztXcu7xk16EC7NzxVcpw9IMuyuRR2HHcKQiZkYtAyROb0
+#     95QnkHY1A+iWbtdhEENc6nU+YybZHFifFAfyGRkhLMntGnzI0vt72KRB+lqMhcDG
+#     bGFHbEw+m2fgUk4oldn/+CkgPNsiUT8ylioPRs1oCbyGEKdNRQhBnfN1kE9VC9oe
+#     oFUnfr2ilSPxJlJ8mfK1nLS9PZesseVMFXuv3QFjIAZYp+rwpAxCu1GX3tIOnXYh
+#     ZwWYdGECgYEA/fOB5cRbozhywd5BqVRXGBLlHZ/27PdjSA79qX4XSNRTeh6oZgUP
+#     arxdaV6wNQJJzv5a+7wnZjsSKdEHRFixlCEGfm+utybs+skAShre1fN3tZtA72s3
+#     diiys2CIKnBwPBahmQKZIC7NAImbDp/JT5C7+pgfZAnnfxJeg+S9wOUCgYEA9wNY
+#     DNFrfSOv2qVwsFEZ0mjvB/nKxk0esKiAcydijwqUf32FRvGi8BuVxWo8V2hUVbxL
+#     FkI1j7Gcvj73HLIjkFlSq4XugF7VBYSCMVaxpfgFcLuRFJMegqPIYBkllM0A7Cj7
+#     noT5Q0Xwhp5/hJxjrkTYYfpJyrbo4Ty/+WbZMQkCgYA0w9C8neTwb/0gHP8FoNmL
+#     zfk/A4q5x9kgEsVP3zwVTNCfchlJQUnnwoWiLCNukDAnyx+6aGjDTAjFGYJqCAe1
+#     oHsJ8EicE4Fvxi3CN/0O9UxtNnnyhRN8AI7IyIjiUIRxZ75h3XFVh4Pe1bia/EZG
+#     uurj8ZGsJoasF3P98ZwfpQKBgCzItGwOg/h6QeXYXOf6rD3TvJKrnTH6xXet9Svr
+#     lhjtCCNdNI66PJpfxfW23x3R0oiAzPRKKL/Wqd5sOA1B1uTS7QQgEi/AAcswTRJ4
+#     71hx8TqRJyqdychRvYB+zqt9x48JlyUUYsTpp0Qg9GmsmP39iWWqJTrVkr9TblLf
+#     bL1RAoGAepEPKv0vn6N9xsz1Owmbvm5FILRUuPKn26xjjnYdd6bXEISCKETEOE7o
+#     KHS+lZMNaDPgSxlxNrgMwIDqHsNBpcVum5VqARasrrXXO+TgxbD2ysHBMi1ruRbs
+#     qL4RYdOSXBcEcJOuoqeBshh6Ms4IqvwSEL2fRv6DcEvA1rPCM+k=
+#     -----END RSA PRIVATE KEY-----'),
+#   'SystemBUID': 'DE366373-309C-42BC-A0C3-4EA67FC8EB08',
+#   'RootCertificate': Data('-----BEGIN CERTIFICATE-----
+#     MIICrTCCAZWgAwIBAgIBADANBgkqhkiG9w0BAQUFADAAMB4XDTE0MTEyMDE0MjMy
+#     MFoXDTI0MTExNzE0MjMyMFowADCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoC
+#     ggEBALGXdk/AII1s+Nw/kBi2k+5Yp1OD773KT3i32/8ljLbkUY9554+Dm1EbDlbf
+#     8IeLCCyDBk/YJswekty+EGHb/fNo6Md32w0STqCztV4E0iP8C4S30n2RTrc9ph/L
+#     gCwDELY07c/CUYKwAt7bWzPCanly/PDCN688fDpyf/mQCWt7FAUFu7C67guoJPKi
+#     Q4OvIgG0YRoSgBs14MtMtqF3Uca2jbLiwffkA+0p6QVUBeXW2/xYO9f7Zq3T+qBl
+#     H+zXFm1l99d1l95jiacryI8EPyhwr5SQlnsw5YxOrh1eHGxywZjiEMnzGKShh5Mi
+#     BlW816zT3ipx6DSLb8kwdn1IVOkCAwEAAaMyMDAwDwYDVR0TAQH/BAUwAwEB/zAd
+#     BgNVHQ4EFgQU4cMMJtHBu6fLT+h6ToAWznZyPi8wDQYJKoZIhvcNAQEFBQADggEB
+#     AIQZALdkkEg1KPJvDeqjM/ZWCvnfFA2ugdGZV6/sAjbrDdocQz2GuRuRzKPZX23P
+#     lyMjnLwtK+k5pvPiPzDIAIxFpncaYG+wVAuwlS5np9X0+MDX0Kv9xcc+ctm9y6Uj
+#     QvLCEOVPgyE4EzEPG+kWIgjV7YfTSFD/ohXA3acgasH54mndlf52arOm0YUj+pXI
+#     t8rFus/RU+7dTKJlTLkpZKv59V6KyBTUVpMBM+4KgczbZJS4gNBdyQDacsOsP7yO
+#     Qiq8B15xW60OMk9aQWyYsdy2U0XdimYhzbYYRSKFOyV4XZD+LqaKKSR/MCatbjny
+#     HXsAx6s75z30yLXbCOS+BQM=
+#     -----END CERTIFICATE-----'),
+# 'WiFiMACAddress': '6c:40:08:df:de:d1'
+#   }
+  
